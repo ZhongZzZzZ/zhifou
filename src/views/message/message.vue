d163949f240978198d87461b477d6cb1d88859ba
zenglin <cenglin@52tt.com>
2019-11-21 15:11:03 +0800

    私信基操v2.0

--- src/views/message/message.vue
+++ src/views/message/message.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="main">  
+    <div class="content">  
         <el-col :span="6">
             <el-menu default-active="1" class="el-menu-vertical-demo">
-                <el-menu-item  v-for="item in users" :key="item.id" @click="chat(item)">
+                <el-menu-item  v-for="item in users" :key="item.id" @click="chat(item)" :index="item.id">
                     <img class="user_avatar" :src="item.url">
                     <span slot="title">{{ item.name }}</span>
                 </el-menu-item>
@@ -11,12 +11,28 @@
         <div class="chat_window" v-if="chatShow">
             <div class="chat_name">{{ current.name }}</div>
             <div class="chat_content">
-                <div class="chat_message" v-for="item in current.message" >
-                    <img :src="current.url" class="message_avatar">
+                <div class="chat_message" v-for="item in current.message" :key="item">
+                    <div class="other_side" v-if="!item.role">
+                        <img :src="current.url" class="message_avatar">
+                        <div class="message_content"> {{ item.content }}</div>
+                        <div class="corr">
+                            <em class="arrline">◆</em>
+                            <span class="arrclr">◆</span>
+                        </div>
+                    </div>
+                    <div class="my_side" v-else>
+                        <div class="message_content"> {{ item.content }}</div>
+                        <img :src="myimg" class="message_avatar">
+                        <div class="corr">
+                            <em class="arrline">◆</em>
+                            <span class="arrclr">◆</span>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div class="chat_textbox">
-
+                <textarea v-model="mymsg" @keyup.enter="onSubmit"></textarea>
+                <el-button type="primary" @click="onSubmit">发送</el-button>
             </div>
         </div>
     </div>
@@ -24,11 +40,14 @@
 
 <script>
     import imgsrc from "../../assets/avatar.jpg"
+    import myimg from "../../assets/unlogin.png"
     export default {
         name: "message",
         data() {
             return {
                 chatShow: false,
+                myimg: myimg,
+                mymsg:'',
                 users: [
                     {id: 1, name:'linhZ', url: imgsrc},
                     {id: 2, name:'hhhhh', url: imgsrc},
@@ -52,6 +71,12 @@
                 this.current.name = cur.name;
                 this.current.url = cur.url;
                 console.log(this.current.url);
+            },
+            onSubmit() {
+                this.current.message.push({
+                     role: 1, content: this.mymsg
+                })
+                this.mymsg = '';
             }
         },
         created(){
@@ -61,7 +86,7 @@
 </script>
 
 <style lang="scss" scoped>
-    .main{
+    .content{
         width: 1000px;
         height: 700px;
         margin: 10px auto;
@@ -76,6 +101,18 @@
     .el-menu {
         min-height: 700px;
     }
+    .el-menu-item.is-active {
+        background-color: #f4f7ff;
+    }
+    .el-menu-item::after {
+        content: "";
+        position: absolute;
+        right: 36px;
+        left: 20px;
+        bottom: 0;
+        height: 1px;
+        background-color: #f7f8fa;
+    }
     .chat_window {
         padding-left: 27%;
         padding-right: 2%;
@@ -88,15 +125,105 @@
         }
         .chat_content {
             height: 420px;
+            overflow-y: scroll;
         }
         .chat_textbox {
             height: 230px;
             border-top: 1px solid #e6e6e6;
         }
-        .message_avatar {
-            width: 50px;
-            height: 50px;
-            border-radius: 50%;
+        .chat_message {
+            margin: 20px 0px;
+            .message_avatar {
+                width: 40px;
+                height: 40px;
+                border-radius: 50%;
+                // border: 1px solid #8DAFFC;
+            }
+            .message_content {
+                background-color: #f6f6f6;
+                // width: 500px;
+                // height: 200px;
+                display: inline-block;
+                vertical-align: top;
+                border-radius: 10px;
+                font-size: 14px;
+                padding: 10px;
+            }
+            .corr {
+                width: 13px;
+                height: 14px;
+                overflow: hidden;
+            }
+            .arrline {
+                color: #f6f6f6;
+                display: block;
+                font-family: "SimSun";
+                font-size: 15px;
+                font-style: normal;
+                font-weight: normal;
+                height: 10px;
+                line-height: normal;
+                text-align: left;
+            }
+            .arrclr {
+                color: #f6f6f6;
+                display: block;
+                font-family: "SimSun";
+                font-size: 15px;
+                font-style: normal;
+                font-weight: normal;
+                height: 10px;
+                line-height: normal;
+            }
+            .other_side {
+                position: relative;
+                .message_content {
+                    margin-left: 10px;
+                }
+                .corr {
+                    position: absolute;
+                    left: 42px;
+                    top: 10px;
+                }
+                .arrclr {
+                    margin: -10px 0 0 1px;
+                }
+            }
+            .my_side {
+                position: relative;
+                margin-right: 20px;
+                text-align: right;
+                .message_content {
+                    margin-right: 10px;
+                }
+                .corr {
+                    position: absolute;
+                    right: 44px;
+                    top: 10px;
+                }
+                .arrclr {
+                    margin: 0 0 0 10px;
+                }
+            }
+        }
+        .chat_textbox {
+            padding-top: 10px;
+            textarea {
+                height: 150px;
+                width: 680px;
+                display: block;
+                padding: 5px 6px 6px;
+                line-height: 19px;
+                resize: none;
+                overflow: auto;
+                outline: none;
+                border: none;
+                font-size: 15px;
+                font-family: inherit;
+            }
+            .el-button {
+                float: right;
+            }
         }
     }
 </style>
