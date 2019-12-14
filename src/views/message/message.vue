6e1713af2a2ef2be961af06f1783ccf47d3fa95c
zenglin <cenglin@52tt.com>
2019-12-14 13:37:14 +0800

    完善私信功能

--- src/views/message/message.vue
+++ src/views/message/message.vue
@@ -1,9 +1,13 @@
 <template>
     <div>
         <Nav style="position:sticky;"></Nav>
-        <div class="content">  
+        <div class="content">
+
             <el-col :span="6">
                 <el-menu default-active="1" class="el-menu-vertical-demo">
+                    <el-menu-item class="search_item">
+                        <el-input placeholder="查找工号" prefix-icon="el-icon-search" v-model="search"></el-input>
+                    </el-menu-item>
                     <el-menu-item  v-for="item in users" :key="item.id" @click="chat(item)" :index="item.id">
                         <img class="user_avatar" :src="item.url">
                         <span slot="title">{{ item.name }}</span>
@@ -12,7 +16,8 @@
             </el-col>
             <div class="chat_window" v-if="chatShow">
                 <div class="chat_name">{{ current.name }}</div>
-                <div class="chat_content">
+                <div class="chat_content" @scroll="onScroll(current.id)">
+                    <div class="loadmore"><i class="el-icon-loading" v-if="!noMore"></i> {{ more_msg }}</div>
                     <div class="chat_message" v-for="item in current.message" :key="item">
                         <div class="other_side" v-if="!item.role">
                             <img :src="current.url" class="message_avatar">
@@ -64,11 +69,11 @@
                 chatShow: false,
                 myimg: myimg,
                 mymsg: '',
-                user : {id: 2},
+                user : {id: '2'},
                 users: [
-                    {id: 3, name:'linhZ', url: imgsrc},
-                    {id: 4, name:'hhhhh', url: imgsrc},
-                    {id: 5, name:'wwwww', url: imgsrc}
+                    {id: '3', name:'linhZ', url: imgsrc},
+                    {id: '4', name:'hhhhh', url: imgsrc},
+                    {id: '5', name:'wwwww', url: imgsrc}
                 ],
                 current:{
                     id: 0,
@@ -78,77 +83,126 @@
                         // { sequence_id: '1', from_id:'3', to_id:'2', role: 0, content: '在吗', create_time: '2019年12月6日 5：21：57'}, 
                         // { sequence_id: '2', from_id:'2', to_id:'3', role: 1, content: '在',create_time: '2019年12月6日 5：22：57'},
                     ],
-                    readymsg: [
+                    readymsg: [ // 未送达的消息
                         // { content: '发给你你却收不到', from_id: 2, to_id: 3 }
-                    ], // 未送达的消息
+                    ], 
                 },
-                tip:'发送中…'
+                tip:'发送中…',
+                more_msg: '查看更多',
+                noMore: false, 
+                search:''
             };
         },
         methods: {
-            chat(cur) {
+            chat(cur) { // 建立对话
                 this.chatShow = true;
                 this.current.id = cur.id;
                 this.current.name = cur.name;
                 this.current.url = cur.url;
+                this.current.message = [];
+                this.current.readymsg = [];
+                this.more_msg = '查看更多';
+                this.noMore = false;
                 console.log(this.current.url);
                 var path = 'ws://192.168.195.9:8123/ws/chat/' + this.user.id + '-' + cur.id + '/';
                 if(typeof(WebSocket) === "undefined") {
                     alert("您的浏览器不支持socket")
                 } else {
                     this.socket = new WebSocket(path) // 实例化socket
-                    console.log(this.socket);
                     this.socket.onopen = this.open // 监听socket连接
                     this.socket.onerror = this.error // 监听socket错误信息
-                    this.socket.onmessage = this.getMessage // 监听socket错误信息
+                    this.socket.onmessage = this.getMessage // 监听socket信息
                 }
+                // let time = new Date()
+                // let chatrecord= { // 第一次请求聊天记录
+                //     from_id: this.user.id,
+                //     to_id: cur.id,
+                //     send_type: 0,
+                //     offset: 1,
+                //     early_time: time.getDate()
+                // }
+                // let ws = this.socket;
+                // ws.addEventListener('open', function () { // 监听websocket open事件
+                //     ws.send(JSON.stringify(chatrecord));
+                // });
             },
+
             open: function () {
                 console.log("socket连接成功")
             },
             error: function () {
                 console.log("连接错误")
             },
-            getMessage: function (msg) { // 接收信息
-                let data = JSON.parse(msg.data);
-                console.log("all:")
-                console.log(data)
-                for(let i in data) {
-                    //console.log(i + ":") 
-                    //console.log(data[0])
-                    if(data[i].role && this.current.readymsg.length) this.current.readymsg.shift();
-                    // console.log(this.current.readymsg)
-                    this.current.message.push(data[i]);
-                } 
-            },
             close: function () {
                 console.log("socket已经关闭")
             },
-            onSubmit(to_id) {
+            getMessage: function (msg) { // 接收信息
+                let data = JSON.parse(msg.data);
+                console.log(data)
+                this.hist_pages = data[0].hist_pages;
+                if(data[0].send_type) this.addmessage(data);
+                else this.addrecord(data);
+            },
+
+            addrecord(record) { // 添加聊天记录
+                if(record[0].is_end) {
+                    this.noMore = true;
+                    this.more_msg = '没有更多了'
+                } else {
+                    if(record[0].is_frist_hist) {
+                        this.current.message = record.concat(this.current.message);
+                        this.scrollToBottom();
+                    } else {
+                        this.current.message = record.concat(this.current.message);
+                    }
+                }
+            },
+            addmessage(msg) { // 添加新信息
+                for(let i in msg) {
+                    if(msg[i].role && this.current.readymsg.length) this.current.readymsg.shift();
+                    this.current.message.push(msg[i]);
+                }
+                this.scrollToBottom();
+            },
+
+            onSubmit(to_id) { // 发送新消息
                 let curmsg = {
                     content: this.mymsg,
                     from_id: this.user.id,
-                    to_id: to_id
+                    to_id: to_id,
+                    send_type: 1
                 }
                 console.log(curmsg);
                 this.socket.send(JSON.stringify(curmsg));
                 this.current.readymsg.push(curmsg)
                 this.mymsg = '';
+                this.scrollToBottom();
             },
-            scrollToBottom() {   // 滑动条保持在最下方
+            onScroll(to_id) { // 上拉加载
+                let container = document.getElementsByClassName('chat_content');
+                let scrollTop = container[0].scrollTop;
+                //console.log('scrollTop:' + scrollTop)
+                if(scrollTop == 0 && !this.noMore) {
+                    let loadmore = {
+                        from_id: this.user.id,
+                        to_id: to_id,
+                        sequence_id: this.current.message[0].sequence_id,
+                        send_type: 0
+                    }
+                    this.socket.send(JSON.stringify(loadmore));
+                }
+            },
+            scrollToBottom() {   // 滑动条在最下方
                 this.$nextTick(() => {
-                    var container = document.getElementsByClassName('chat_content');
+                    let container = document.getElementsByClassName('chat_content');
                     container[0].scrollTop = container[0].scrollHeight;
                 })
-            }
+            },
         },
         created(){
-            
+
         },
-        updated() {
-            this.scrollToBottom();
-        },
-        destroyed () {
+        destroyed() {
             // 销毁监听
             this.socket.onclose = this.close
         },
@@ -185,6 +239,12 @@
         height: 1px;
         background-color: #f7f8fa;
     }
+    .search_item:hover {
+        background-color: #fff;
+    }
+    .search_item::after {
+        height: 0;
+    }
     .chat_window {
         padding-left: 27%;
         padding-right: 2%;
@@ -198,6 +258,10 @@
         .chat_content {
             height: 420px;
             overflow-y: auto;
+        }
+        .loadmore {
+            color:#d6d6d6;
+            text-align: center;
         }
         .chat_textbox {
             height: 230px;
@@ -279,7 +343,7 @@
                 .tip {
                     display: inline-block;
                     vertical-align: bottom;
-                    color: #e6e6e6;
+                    color: #d6d6d6;
                 }
             }
         }
